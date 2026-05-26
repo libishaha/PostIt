f7632f08d8893b893c9fd34796bdeaf181173c85
Libisha Shirani <libisha01@gmail.com>
2026-05-27 01:46:28 +0530

    Implement FastAPI authentication and signup endpoint

---
+++ backend/main.py
@@ -0,0 +1,97 @@
+from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from database import Base, engine, get_db
+from models import User, Post
+import os
+from dotenv import load_dotenv
+from passlib.context import CryptContext
+from pydantic import BaseModel, EmailStr
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials = True,
+    allow_origins = ["http://localhost:5173"],
+    allow_headers = ["*"],
+    allow_methods = ["*"]
+)
+
+#create tables on startup
+Base.metadata.create_all(bind = engine)
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+
+class UserSignup(BaseModel):
+    email: EmailStr
+    username: str
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class PostCreate(BaseModel):
+    title: str
+    content: str
+
+class PostResponse(BaseModel):
+    id: int
+    title: str
+    content: str
+    created_at: datetime
+    owner_id: int
+
+    class Config:
+        from_attributes = True
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain: str, hashed: str):
+    return pwd_context.verify(plain, hashed)
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp" : expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(token: str, db: Session):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        user = db.query(User).filter(User.email == email).first()
+        if user is None:
+            raise HTTPException(status_code=401, detail="User not found")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
+@app.post("/signup")
+def signup(data: UserSignup, db: Session = Depends(get_db)):
+    if db.query(User).filter(User.email == data.email).first():
+        raise HTTPException(status_code=401, detail="Email already registered")
+    if db.query(User).filter(User.username == data.username).first():
+        raise HTTPException(status_code=401, detail="Username already taken")
+    
+    user = User(
+        username=data.username,
+        email=data.email,
+        hashed_password=hash_password(data.password)
+    )
+
+    db.add(user)
+    db.commit()
+    return {"message" : "Account Created Successfully"}
