812373cd83dacb6b158e85deca6f0fddfa0a0221
Libisha Shirani <libisha01@gmail.com>
2026-05-27 13:39:21 +0530

    Added login endpoint

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM')
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
 
@@ -37,7 +37,7 @@ class UserSignup(BaseModel):
     password: str
 
 class UserLogin(BaseModel):
-    username: str
+    email: EmailStr
     password: str
 
 class PostCreate(BaseModel):
@@ -95,3 +95,16 @@ def signup(data: UserSignup, db: Session = Depends(get_db)):
     db.add(user)
     db.commit()
     return {"message" : "Account Created Successfully"}
+
+@app.post("/login")
+def login(data: UserLogin, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == data.email).first()
+    if not user or not verify_password(plain=data.password, hashed=user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    
+    token = create_token({"sub" : user.email})
+    return {
+        "access_token" : token,
+        "token_type" : "bearer",
+        "username" : user.username
+    }
